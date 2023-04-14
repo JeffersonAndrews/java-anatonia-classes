78d09bdfd7c9722e7e235332402938e4c94c3bf5
Jefferson Andrews <110488622+JeffersonAndrews@users.noreply.github.com>
2023-04-14 20:19:49 -0300

    explicação final em variaveis

---
+++ src/tiposdevariaves/TiposVariaveis.java
@@ -0,0 +1,20 @@
+package tiposdevariaves;
+
+public class TiposVariaveis {
+
+	public static void main(String[] args) {
+		
+		double salarioMinimo = 2500;
+		
+		short numeroCurto = 1;
+		int numeroNormal = numeroCurto;
+		short nuneroCurto2 = (short) numeroNormal;
+
+		
+	final int numero = 5; 
+	// não muda nunca
+		
+		System.out.println(numero);
+	}
+
+}
